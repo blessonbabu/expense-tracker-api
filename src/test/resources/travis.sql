d920e2d44ee131a74302c37e5dd29e6d4b7e3e80
blessonbabu <blessonbabu8129@gmail.com>
2017-11-01 15:39:56 +0530

    added sql file

---
+++ src/test/resources/travis.sql
@@ -0,0 +1,14 @@
+# Create Testuser
+
+CREATE DATABASE IF NOT EXISTS `expensetracker` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `expensetracker`;
+# Create Table
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` Long(11) NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `password` varchar(256) DEFAULT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+# Add Data
+INSERT INTO `user` (`id`, `email`, `password`) VALUES (1, "test@test.com", "test")

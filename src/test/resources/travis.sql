83a2987a629d24927bca51c1a0c9e0d959c13aa3
BLESSON BABU <blessonbabu8129@gmail.com>
2017-11-01 15:46:00 +0530

    Update travis.sql

--- src/test/resources/travis.sql
+++ src/test/resources/travis.sql
@@ -8,7 +8,7 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` Long(11) NOT NULL,
   `email` varchar(50) DEFAULT NULL,
-  `password` varchar(256) DEFAULT NULL,
+  `password` varchar(256) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 # Add Data
-INSERT INTO `user` (`id`, `email`, `password`) VALUES (1, "test@test.com", "test")
+INSERT INTO `user` (`id`, `email`, `password`) VALUES (1, "test@test.com", "test");
